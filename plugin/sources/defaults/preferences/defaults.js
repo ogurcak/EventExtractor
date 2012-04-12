a5edb65df6ee8c372e0fbbc963d65c2f817a7eb5
ogurcak <filip.ogurcak@gmail.com>
2012-04-12 14:20:18 +0200

    DELETED

--- plugin/sources/defaults/preferences/defaults.js
+++
@@ -1,5 +0,0 @@
-pref("extensions.wrtranslator.language", "en-es");
-pref("extensions.wrtranslator.firstexec", true);
-pref("extensions.wrtranslator.floating_link_enable", true);
-pref("extensions.wrtranslator.floating_link_time", 2000);
-pref("extensions.wrtranslator.status_visible", true);
