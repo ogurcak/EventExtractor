48243e7aa90769b2c563a27f0d4c66e2d09ea73e
Filip Ogurcak <filip.ogurcak@gmail.com>
2013-01-10 22:09:44 +0100

    refactoring

--- autoexec.bat
+++ autoexec.bat
@@ -1,5 +1,7 @@
 MD bin
 
-"C:\Program Files\Java\jdk1.7.0_07\bin\javac" -d bin -encoding UTF-8 -cp lib\*;..\GATE\lib\*;..\GATE\bin\*;extraction_methods\* src\Extractor\* src\Server\*
+"C:\Program Files\Java\jdk1.7.0_10\bin\javac" -d bin -encoding UTF-8 -cp lib\*;..\GATE\lib\*;..\GATE\bin\*;extraction_methods\* src\Extractor\* src\Server\* src\Server\Method\* src\Server\POSTActions\*
 
-"C:\Program Files\Java\jdk1.7.0_07\bin\java" -Dlog4j.configuration=file:log4j/log4j.properties -cp bin\;lib\*;..\GATE\lib\*;..\GATE\bin\*;extraction_methods\*; Server.Main 5000 -Dfile.encoding=UTF-8 -Djava.security.manager -Djava.security.policy=distr/java.policy
+
+
+"C:\Program Files\Java\jdk1.7.0_10\bin\java" -Dlog4j.configuration=file:log4j/log4j.properties -cp bin\;lib\*;..\GATE\lib\*;..\GATE\bin\*;extraction_methods\*; Server.Main 5000 -Dfile.encoding=UTF-8 -Djava.security.manager -Djava.security.policy=distr/java.policy
